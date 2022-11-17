90869ba7da2076d9389a0398b27a8cb2a52dfeeb
HOTBRAINY <brainhg95@gmail.com>
2022-11-17 04:49:34 -0800

    Update mapping.ts

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,7 +3,6 @@ import { MarketItem } from '../generated/schema'
 
 export function handleMarketItemCreated(event: MarketItemCreated): void {
   let marketItem = new MarketItem(event.params.id.toHex())
-  // marketItem.tokenId = event.params.tokenId
   marketItem.seller = event.params.seller
   marketItem.owner = event.params.owner
   marketItem.price = event.params.price
